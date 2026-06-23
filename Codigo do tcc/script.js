3061348ef30e30cc51fc294cc30c1ade74de346f
Victória <Victoria.pacheco2606@gmail.com>
2026-06-23 16:22:55 -0300

    Add files via upload
    guia de pesquisa feito
    
    Signed-off-by: Victória <Victoria.pacheco2606@gmail.com>

--- Codigo do tcc/script.js
+++ Codigo do tcc/script.js
@@ -1,15 +1,15 @@
 /*modo escuro e lupa de pesquisa*/
 const body = document.querySelector("body"),
- nav = document.querySelector("nav"),
- modeToggle = document.querySelector(".dark-light"),
-  searchToggle = document.querySelector(".searchToggle");
+    nav = document.querySelector("nav"),
+    modeToggle = document.querySelector(".dark-light"),
+    searchToggle = document.querySelector(".searchToggle");
 
-
-   modeToggle.addEventListener("click", () => {
+modeToggle.addEventListener("click", () => {
     modeToggle.classList.toggle("active");
     body.classList.toggle("dark");
 });
+
 /*js code to toggle search box*/
-searchToggle.addEventListener("clik", () => {
+searchToggle.addEventListener("click", () => {
     searchToggle.classList.toggle("active");
-   });
+});
